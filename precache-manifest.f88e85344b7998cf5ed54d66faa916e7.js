cb39b0442f864151d7d226f1b2f8112d4de7521a
Raman <oneoftheelk@gmail.com>
2019-10-06 11:43:57 +0300

    Updates

---
+++ precache-manifest.f88e85344b7998cf5ed54d66faa916e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb2cee7e0e66ac710ebc661b21522cb",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "984f459d50dae3d34815",
+    "url": "/counter/static/css/main.0f536dde.chunk.css"
+  },
+  {
+    "revision": "87ba21db6372e7863851",
+    "url": "/counter/static/js/2.b70f76e9.chunk.js"
+  },
+  {
+    "revision": "984f459d50dae3d34815",
+    "url": "/counter/static/js/main.3c90efef.chunk.js"
+  },
+  {
+    "revision": "57c1768b7baad448290d",
+    "url": "/counter/static/js/runtime~main.27fcea12.js"
+  },
+  {
+    "revision": "266c44f4d5e4b96af11fed54448b3ed1",
+    "url": "/counter/static/media/background-image.266c44f4.jpg"
+  }
+]);
